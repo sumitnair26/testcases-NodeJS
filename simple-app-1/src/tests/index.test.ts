ddba778d97e8caa5d8fdcc6d424468797df0538d
sumitnair26 <sumit.nair26@gmail.com>
2024-05-12 19:35:57 +0530

    writing first test case

--- simple-app-1/src/tests/index.test.ts
+++ simple-app-1/src/tests/index.test.ts
@@ -1,8 +1,15 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect, it, test} from '@jest/globals';
 import {sum} from '../index';
 
 describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+  it('should sum 1 + 2 correctly', () => {
+    const finalOutput = sum(1,2);
+    expect(finalOutput).toBe(3);
+    //expect(sum(1, 2)).toBe(3);
   });
+
+  it('should return sum of negative numver correctlt', () =>{
+    const finalOutput = sum(-1,-2);
+    expect(finalOutput).toBe(-3);
+  })
 });
